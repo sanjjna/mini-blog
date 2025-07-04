e20fcc25500477b5a313fc030ac89aa7c32e5b40
sanjjna <sanjanaverma144@gmail.com>
2025-07-04 17:27:25 +0530

    Add CORS config for frontend

--- server/server.js
+++ server/server.js
@@ -7,32 +7,27 @@ const postRoutes = require('./routes/postRoutes');
 
 dotenv.config();
 const app = express();
+const allowedOrigins = ['https://mini-blog-frontend-y0uw.onrender.com'];
 
-// 🔐 CORS config (important for cross-origin frontend like Vercel)
-const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'https://mini-blog-frontend-w4v2.onrender.com',
-  credentials: true,
-};
-app.use(cors(corsOptions));
 
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
 
-// Test route
 app.get('/', (req, res) => {
   res.send('Blog API running...');
 });
 
-// DB connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('MongoDB Connected'))
-  .catch(err => console.error('MongoDB Connection Error:', err));
+  .catch(err => console.error(err));
 
-// Server start
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
